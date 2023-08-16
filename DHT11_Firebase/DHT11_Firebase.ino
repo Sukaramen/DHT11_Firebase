171b9bdd2e6e13766c1c2c36225e6e9ac3ba5941
Sukaramen <117975057+Sukaramen@users.noreply.github.com>
2023-08-16 16:09:06 +0700

    Add files via upload

---
+++ DHT11_Firebase/DHT11_Firebase.ino
@@ -0,0 +1,47 @@
+#include "FirebaseESP8266.h"
+#include <ESP8266WiFi.h>
+#include <DHT.h>
+int sen;
+
+//................SSID DAN PASS PADA WIFI................//
+const char* ssid = "*********";
+const char* pass = "*********";
+
+//................HOST DAN TOKEN PADA FIREBASE................//
+#define FIREBASE_HOST "*********"
+#define FIREBASE_TOKEN "*********"
+
+FirebaseData DHTData;
+
+#define DHTTYPE DHT11
+#define DHTPIN D2 //pin DHT yang dipakai
+DHT dht(DHTPIN, DHTTYPE);
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  dht.begin();
+  WiFi.begin(ssid, pass);
+  Serial.println("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(100);
+    Serial.println("Tunggu.");
+  }
+  Serial.println();
+  Serial.print("IP : ");
+  Serial.println(WiFi.localIP());
+  Firebase.begin(FIREBASE_HOST, FIREBASE_TOKEN);
+  Firebase.reconnectWiFi(true);
+}
+void loop() {
+  float t = dht.readTemperature();
+  float h = dht.readHumidity();
+  Serial.print("Temperature : ");
+  Serial.print(t);
+  Serial.println("C");
+  Serial.print("Humidity : ");
+  Serial.print(H);
+  Serial.println("%"); 
+  Firebase.setFloat(TestData, "/data/send", t); //Path pada database Firebase contoh /data/send
+  Firebase.setFloat(TestData, "/data/send", h); //Path pada database Firebase contoh /data/send
+}
